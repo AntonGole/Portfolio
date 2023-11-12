d294236b93a1578d253254e2f70e7dcbcd793c78
AntonGole <gole@net.chalmers.se>
2023-11-12 21:50:23 +0100

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import Message from './Message';
+import ListGroup from "./components/ListGroup";
+import { MouseEvent } from "react";
 
 function App() {
-  return <div><Message></Message></div>;
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {}
+      <ul className="list-group">
+        {items.length == 0 && <p>No item found</p>}
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
