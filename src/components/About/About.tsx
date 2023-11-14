63efeee579f7f289df475debdc3dd5e982081bfb
AntonGole <gole@net.chalmers.se>
2023-11-14 18:36:50 +0100

    Added an about section and removed some old files

---
+++ src/components/About/About.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience of developing software in languages such as
+                Python, Java and C++ both from personal projects as well as from
+                school projects shown down below
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI/Frontend</h3>
+              <p>
+                I have experience designing applications with well thought
+                through UX designs
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
